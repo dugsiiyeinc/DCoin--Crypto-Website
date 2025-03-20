07cb1cffa27cea65adc72579d990cf39138967f7
Masud Mohamed <masudmohamed2024@gmail.com>
2025-03-20 23:23:48 +0200

    Add crypto price tracker section functionality

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const humbergur = document.querySelector('.humburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const icons = document.querySelectorAll('i');
 const tableBody = document.querySelector('#cryptoTable tbody');
+const cryptoTable_2 = document.querySelector('#cryptoTable-2 tbody');
+const priceFilter = document.querySelector('#price-filter');
 
 humbergur.addEventListener("click", function (event){
     const isVisible = mobileMenu.getAttribute('data-visible');
@@ -26,7 +28,7 @@ async function fetchCryptoData(){
     allData = data;
     console.log(data);
     displayData(data);
-    
+    displayData_2(data)
     
     
 }
@@ -48,5 +50,45 @@ function displayData(data){
 
     
 }
+
+// Crypto Price Tracker
+function displayData_2(data){
+    cryptoTable_2 .innerHTML ='';
+    data.forEach(coin =>{
+        const row = document.createElement('tr');
+        row.innerHTML = `
+         <td>${coin.market_cap_rank}</td>
+        <td>${coin.name}</td>
+        <td>${coin.current_price}</td>
+        
+
+            `;
+            cryptoTable_2 .appendChild(row);
+    });
+
+
+    
+}priceFilter.addEventListener('change', () => {
+    let filterData;
+    
+    if (priceFilter.value === 'all') {
+        filterData = allData;
+    } else {
+        filterData = allData.filter(coin => {
+            const price = coin.current_price;
+            if (priceFilter.value === 'below1') return price <= 1;
+            if (priceFilter.value === 'below5') return price > 1 && price <= 5;
+            if (priceFilter.value === 'below10') return price > 5 && price <= 10;
+            if (priceFilter.value === 'below100') return price > 10 && price <= 100;
+            if (priceFilter.value === 'below500') return price > 100 && price <= 500;
+            if (priceFilter.value === 'below1000') return price > 500 && price <= 1000;
+            if (priceFilter.value === 'above-1k') return price >1000;
+            
+        });
+    }
+
+    
+    displayData_2(filterData);
+});
 
 fetchCryptoData()
