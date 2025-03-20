d515ab04335d1c786ebb9c72759fdeeb19eeeeae
Masud Mohamed <masudmohamed2024@gmail.com>
2025-03-20 22:59:54 +0200

    Add crypto currencies api

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const humbergur = document.querySelector('.humburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const icons = document.querySelectorAll('i');
+const tableBody = document.querySelector('#cryptoTable tbody');
 
 humbergur.addEventListener("click", function (event){
     const isVisible = mobileMenu.getAttribute('data-visible');
@@ -17,3 +18,35 @@ humbergur.addEventListener("click", function (event){
         icons[1].setAttribute('data-visible', "true");
     }
  })
+
+ //  Crypto currenciesn section
+async function fetchCryptoData(){
+    const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd');
+    const data = await response.json();
+    allData = data;
+    console.log(data);
+    displayData(data);
+    
+    
+    
+}
+
+function displayData(data){
+    tableBody.innerHTML ='';
+    data.forEach(coin =>{
+        const row = document.createElement('tr');
+        row.innerHTML = `
+         <td>${coin.market_cap_rank}</td>
+        <td>${coin.name}</td>
+        <td>${coin.current_price}</td>
+       
+
+            `;
+        tableBody.appendChild(row);
+    });
+
+
+    
+}
+
+
